6ac72fb3c3810525df588d2477af47a48105e0f4
Youlserf Cardenas <75793798+youlserf@users.noreply.github.com>
2024-12-03 21:20:26 -0500

    fix: remove swagger annotations

--- src/main/java/com/kallpacode/reactivemysql/ReactiveMysqlApplication.java
+++ src/main/java/com/kallpacode/reactivemysql/ReactiveMysqlApplication.java
@@ -1,7 +1,5 @@
 package com.kallpacode.reactivemysql;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
